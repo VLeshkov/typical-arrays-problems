6a2acb619f4c525052aece05c6adeca589d42913
Victor Leshkov <7065531+VLeshkov@users.noreply.github.com>
2021-08-15 19:36:51 +0600

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
 exports.min = function min (array) {
+  if (array && array.length > 0) {
+    let minNum = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (minNum > array[i] ){
+        minNum = array[i];
+      }
+    }
+    return minNum;
+  }
   return 0;
 }
 
 exports.max = function max (array) {
+  if (array && array.length > 0) {
+    let maxNum = array[0];
+    for (let i = 1; i < array.length; i++) {
+      if (maxNum < array[i]) {
+        maxNum = array[i];
+      }
+    }
+    return maxNum;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length > 0) {
+    let avgNum = 0;
+    for (let i = 0; i < array.length; i++) {
+        avgNum += array[i];
+      }
+    return avgNum / array.length;
+  }
   return 0;
 }
